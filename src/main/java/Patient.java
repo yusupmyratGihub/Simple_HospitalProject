671fcae219bd9d767e859030922b5a9316ab0c7f
Yusupmyrat <ashgabatmekan@gmail.com>
2022-05-19 00:01:25 +0200

    v1.

--- src/main/java/Patient.java
+++ src/main/java/Patient.java
@@ -2,4 +2,23 @@ public class Patient extends Employee {
     private int patientID;
 
 
+    public Patient(int personelID, String fullName, String lastName, String country, String birthDate, String gender, String adress, int phone, String email, int patientID) {
+        super(personelID, fullName, lastName, country, birthDate, gender, adress, phone, email);
+        this.patientID = patientID;
+    }
+
+    public int getPatientID() {
+        return patientID;
+    }
+
+    public void setPatientID(int patientID) {
+        this.patientID = patientID;
+    }
+
+    @Override
+    public String toString() {
+        return "Patient{" +
+                "patientID=" + patientID +
+                "} " + super.toString();
+    }
 }
